64c2144fa30d9de1ec0c9c17f34a6a26e63dee33
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2023-03-31 17:44:14 +0530

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from fastapi import FastAPI
-from Scrapping_Service.router import router as stock_scrapper
-
-# create API app object
-app = FastAPI()
-
-
-app.include_router(stock_scrapper)
